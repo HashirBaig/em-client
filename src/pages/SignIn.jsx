3f03b564fb5cf30016edbdea753ed3e217d4ee11
Hashir Baig <hashirbaig456@gmail.com>
2023-01-23 17:47:26 +0500

    auth 2

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,9 +1,50 @@
+import { useForm } from "react-hook-form"
+import * as yup from "yup"
+import { yupResolver } from "@hookform/resolvers/yup"
+import { useToast } from "@chakra-ui/react"
 import RequiredFieldMark from "../components/RequiredFieldMark"
 
+const schema = yup.object().shape({
+  email: yup.string().required("Email is required"),
+  password: yup.string().required("Password is required"),
+})
+
 function SignIn() {
+  const toast = useToast()
+
+  const { register, handleSubmit } = useForm({
+    mode: "onSubmit",
+    resolver: yupResolver(schema),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  })
+
+  const onSubmit = formData => {
+    try {
+      console.log("form-data: ", formData)
+      toast({
+        title: "Sign in Successfull",
+        description: "User signed in successfully",
+        status: "success",
+        duration: 4000,
+      })
+    } catch (error) {
+      console.log(error)
+      toast({
+        title: "Can't sign in",
+        description: error?.message || error,
+        status: "error",
+        duration: 4000,
+        isClosable: true,
+      })
+    }
+  }
+
   return (
     <div className="min-h-screen flex items-center justify-center">
-      <form className="auth-form">
+      <form className="auth-form" onSubmit={handleSubmit(onSubmit)}>
         <div className="row justify-center">
           <img src="/assets/svgs/condensed_logo.svg" alt="EM Logo" />
         </div>
@@ -13,7 +54,7 @@ function SignIn() {
               <label htmlFor="email">Email</label>
               <RequiredFieldMark />
             </div>
-            <input type="text" id="email" autoComplete="off" placeholder="abc@example.com" />
+            <input type="text" id="email" autoComplete="off" placeholder="abc@example.com" {...register("email")} />
           </div>
         </div>
         <div className="row">
@@ -22,7 +63,7 @@ function SignIn() {
               <label htmlFor="password">Password</label>
               <RequiredFieldMark />
             </div>
-            <input type="password" id="password" autoComplete="off" />
+            <input type="password" id="password" autoComplete="off" {...register("password")} />
           </div>
         </div>
         <div className="row">
--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,9 +1,51 @@
+import { useForm } from "react-hook-form"
+import * as yup from "yup"
+import { yupResolver } from "@hookform/resolvers/yup"
+import { useToast } from "@chakra-ui/react"
 import RequiredFieldMark from "../components/RequiredFieldMark"
 
+const schema = yup.object().shape({
+  email: yup.string().required("Email is required"),
+  password: yup.string().required("Password is required"),
+})
+
 function SignUp() {
+  const toast = useToast()
+
+  const { register, handleSubmit } = useForm({
+    mode: "onSubmit",
+    resolver: yupResolver(schema),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  })
+
+  const onSubmit = formData => {
+    try {
+      console.log("form-data: ", formData)
+      toast({
+        title: "Account created successfully",
+        description: "Account created successfully",
+        status: "success",
+        duration: 4000,
+        isClosable: true,
+      })
+    } catch (error) {
+      console.log(error)
+      toast({
+        title: "Can't create an account",
+        description: error?.message || error,
+        status: "error",
+        duration: 4000,
+        isClosable: true,
+      })
+    }
+  }
+
   return (
     <div className="min-h-screen flex items-center justify-center">
-      <form className="auth-form">
+      <form className="auth-form" onSubmit={handleSubmit(onSubmit)}>
         <div className="row justify-center">
           <img src="/assets/svgs/condensed_logo.svg" alt="EM Logo" />
         </div>
@@ -13,7 +55,7 @@ function SignUp() {
               <label htmlFor="email">Email</label>
               <RequiredFieldMark />
             </div>
-            <input type="text" id="email" autoComplete="off" placeholder="abc@example.com" />
+            <input type="text" id="email" autoComplete="off" placeholder="abc@example.com" {...register("email")} />
           </div>
         </div>
         <div className="row">
@@ -22,7 +64,7 @@ function SignUp() {
               <label htmlFor="password">Password</label>
               <RequiredFieldMark />
             </div>
-            <input type="password" id="password" autoComplete="off" />
+            <input type="password" id="password" autoComplete="off" {...register("password")} />
           </div>
         </div>
         <div className="row">
@@ -31,7 +73,7 @@ function SignUp() {
               <label htmlFor="confirmPassword">Confirm Password</label>
               <RequiredFieldMark />
             </div>
-            <input type="password" id="confirmPassword" autoComplete="off" />
+            <input type="password" id="confirmPassword" autoComplete="off" {...register("confirmPassword")} />
           </div>
         </div>
         <div className="row">
