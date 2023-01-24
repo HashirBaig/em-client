eef31b220979640bba024eb9a6a6514fd2dafb7a
Hashir Baig <hashirbaig456@gmail.com>
2023-01-24 11:51:43 +0500

    Minor fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,9 @@
-// import { ColorModeSwitcher } from "./ColorModeSwitcher"
-import { useState, useEffect } from "react"
 import { ChakraProvider, theme } from "@chakra-ui/react"
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
-import { SplashScreen, Home, SignIn, SignUp } from "./pages"
+import { Home, SignIn, SignUp } from "./pages"
 import { AllRoutesMap } from "./routes/RoutesConfig"
 
 function App() {
-  const [isLoading, setIsLoading] = useState(false)
-
-  useEffect(() => {
-    setIsLoading(true)
-
-    setTimeout(() => {
-      setIsLoading(false)
-    }, 1500)
-
-    // eslint-disable-next-line
-  }, [])
-
-  // if (isLoading) return <SplashScreen />
-
   return (
     <ChakraProvider theme={theme}>
       <Router>
