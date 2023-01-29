0d56a55adaf2570085c0ae1ea34451cdbec0f3b2
Hashir Baig <hashirbaig456@gmail.com>
2023-01-29 17:46:06 +0500

    Adjustments to landing page

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -4,16 +4,10 @@ import { AllRoutesMap } from "../routes/RoutesConfig"
 function Landing() {
   return (
     <div className="min-h-screen flex items-center justify-center">
-      <div className="space-y-3">
-        <div>
-          <span className="text-4xl text-blue-600 font-semibold">Landing</span>
-          <span className="text-4xl text-gray-300 font-semibold">Page</span>
-        </div>
-        <div className="text-center py-4">
-          <Link className="btn btn-outline px-4" to={AllRoutesMap?.login}>
-            Get Started
-          </Link>
-        </div>
+      <div className="text-center py-4">
+        <Link className="btn btn-outline px-4" to={AllRoutesMap?.login}>
+          Get Started
+        </Link>
       </div>
     </div>
   )
